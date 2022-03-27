891fd0df7c419cae0c055bc07ccac24eca88edb7
Haley Telega <haleytelega@outlook.com>
2022-03-27 19:26:29 -0400

    trying to get city name to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-var apiKey = fbd00966461c8849ae90f0d8b2f1332f;
+var userFormEl = document.querySelector("#city-form");
+var apiKey = "fbd00966461c8849ae90f0d8b2f1332f";
+
+var getCity = function () {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city.name + "&appid=" + apiKey;
+    // fetch(apiKey).then(function(response) {
+    //     if (response.ok) {
+    //         response.json().then(function(data){
+                console.log(apiUrl)
+                console.log("is this working?");
+
+
+};
+
+userFormEl.addEventListener("submit", getCity);
