0f830659d4d577a60bc743329c7ea31622fc07b4
Haley Telega <haleytelega@outlook.com>
2022-03-28 15:28:00 -0400

    got getting the city to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
 var userFormEl = document.querySelector("#city-form");
 var apiKey = "fbd00966461c8849ae90f0d8b2f1332f";
 
-var getCity = function () {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city.name + "&appid=" + apiKey;
+function getCity (city) {
+    // console.log(city);
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data){
-                console.log(city.name, data)
+                console.log(city, data)
             });
         }
     });
 };
 
-userFormEl.addEventListener("submit", getCity);
+userFormEl.addEventListener("submit", function(e) {
+    e.preventDefault();
+    var inputEl = document.getElementById("city").value;
+    console.log(inputEl);
+    getCity(inputEl);
+});
