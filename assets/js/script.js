40fa555a89f38366f85c4d93c444ade7faea4e3e
Haley Telega <haleytelega@outlook.com>
2022-03-28 13:38:55 -0400

    trying to get city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var apiKey = "fbd00966461c8849ae90f0d8b2f1332f";
 
 var getCity = function () {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city.name + "&appid=" + apiKey;
-    fetch(apiKey).then(function(response) {
+    fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data){
                 console.log(city.name, data)
