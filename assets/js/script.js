ed5f6d32460203e0f4bbecf9f8c560e328884c88
Haley Telega <haleytelega@outlook.com>
2022-04-01 19:55:49 -0400

    got 5 days worth of weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 var userFormEl = document.querySelector("#city-form");
 var cityContainerEl = document.querySelector("#city-container");
 var cityInputEl = document.querySelector("#city");
-var fiveDayEl = document.querySelector(".five");
+var dayOneEl = document.querySelector("#day1");
+var dayTwoEl = document.querySelector("#day2");
+var dayThreeEl = document.querySelector("#day3");
+var dayFourEl = document.querySelector("#day4");
+var dayFiveEl = document.querySelector("#day5");
 
 var apiKey = "fbd00966461c8849ae90f0d8b2f1332f";
 
@@ -51,18 +55,90 @@ function getCity (city) {
             if(response.ok) {
             response.json().then(function(data) {
             console.log(data);
-            for (i = 0; i < fiveDayEl.length; i++) {
-                fiveDayEl.textContent = "";
-                var day = i*8
-                var temp = data.list[day].main.temp;
-                var wind = data.list[day].wind.speed;
-                var humidity = data.list[day].main.humidity;
+                //day 1
+                var oneTemp = data.list[0].main.temp;
+                var oneWind = data.list[0].wind.speed;
+                var oneHumidity = data.list[0].main.humidity;
 
-                var fiveTemp = document.createElement("p");
-                fiveTemp.textContent = "Temp: " + temp;
+                var tempOne = document.createElement("p");
+                tempOne.textContent = "Temp: " + oneTemp;
+                oneEl.append(oneTemp);
 
-                fiveEl[i].appendChild(fiveTemp);
-            }
+                var windOne = document.createElement("p");
+                windOne.textContent = "Wind: " + oneWind;
+                oneEl.appendChild(windOne);
+
+                var humidityOne = document.createElement("p");
+                humidityOne.textContent = "Humidity: " + oneHumidity;
+                oneEl.appendChild(humidityOne);
+
+                //day 2
+                var twoTemp = data.list[8].main.temp;
+                var twoWind = data.list[8].wind.speed;
+                var twoHumidity = data.list[8].main.humidity;
+
+                var tempTwo = document.createElement("p");
+                tempTwo.textContent = "Temp: " + twoTemp;
+                twoEl.append(tempTwo);
+
+                var windTwo = document.createElement("p");
+                windTwo.textContent = "Wind: " + twoWind;
+                twoEl.appendChild(windTwo);
+
+                var humidityTwo = document.createElement("p");
+                humidityTwo.textContent = "Humidity: " + twoHumidity;
+                twoEl.appendChild(humidityTwo);
+
+                //day 3
+                var threeTemp = data.list[16].main.temp;
+                var threeWind = data.list[16].wind.speed;
+                var threeHumidity = data.list[16].main.humidity;
+
+                var tempThree = document.createElement("p");
+                tempThree.textContent = "Temp: " + threeTemp;
+                threeEl.append(tempThree);
+
+                var windThree = document.createElement("p");
+                windThree.textContent = "Wind: " + threeWind;
+                threeEl.appendChild(windThree);
+
+                var humidityThree = document.createElement("p");
+                humidityThree.textContent = "Humidity: " + threeHumidity;
+                threeEl.appendChild(humidityThree);
+
+                //day 4
+                var fourTemp = data.list[24].main.temp;
+                var fourWind = data.list[24].wind.speed;
+                var fourHumidity = data.list[24].main.humidity;
+
+                var tempFour = document.createElement("p");
+                tempFour.textContent = "Temp: " + fourTemp;
+                fourEl.append(tempFour);
+
+                var windFour = document.createElement("p");
+                windFour.textContent = "Wind: " + fourWind;
+                fourEl.appendChild(windFour);
+
+                var humidityFour = document.createElement("p");
+                humidityFour.textContent = "Humidity: " + fourHumidity;
+                fourEl.appendChild(humidityFour);
+
+                //day 5
+                var fiveTemp = data.list[32].main.temp;
+                var fiveWind = data.list[32].wind.speed;
+                var fiveHumidity = data.list[32].main.humidity;
+
+                var tempFive = document.createElement("p");
+                tempFive.textContent = "Temp: " + fiveTemp;
+                fiveEl.append(tempFive);
+
+                var windFive = document.createElement("p");
+                windFive.textContent = "Wind: " + fiveWind;
+                fiveEl.appendChild(windFive);
+
+                var humidityfive = document.createElement("p");
+                humidityfive.textContent = "Humidity: " + fiveHumidity;
+                fiveEl.appendChild(humidityfive);
             });
         }
     });
@@ -81,8 +157,20 @@ function getCity (city) {
 
     cityContainerEl.appendChild(cityEl);
 
+    var oneEl = document.createElement("a");
+    dayOneEl.append(oneEl);
+
+    var twoEl = document.createElement("a");
+    dayTwoEl.appendChild(twoEl);
+
+    var threeEl = document.createElement("a");
+    dayThreeEl.appendChild(threeEl);
+
+    var fourEl = document.createElement("a");
+    dayFourEl.appendChild(fourEl);
+
     var fiveEl = document.createElement("a");
-    fiveDayEl.appendChild(fiveEl);
+    dayFiveEl.appendChild(fiveEl);
     } 
     });
 }
